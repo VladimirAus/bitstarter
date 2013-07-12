505d9fb61a1ecf94d139541b28d75268b973d2be
Vladimir Roudakov <v_roudakov@yahoo.com>
2013-07-12 13:06:18 +0000

    Adding URL support

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+// var URI_DEFAULT = "http://gentle-bayou-6766.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,8 +38,40 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertUrlExists = function(url, checks) {
+    restler.get(url).on('complete', function(result) {
+      if (result instanceof Error) {
+        // sys.puts('Error: ' + result.message);
+        this.retry(5000); // try again after 5 sec
+        console.log("URL %s returned the following error: %s. Exiting.", url, result.message);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+      } else {
+        
+          outputResult(cheerioHtmlContent(result), checks);
+
+      }
+    });
+
+  /*  var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;*/
+};
+
+var outputResult = function(content, checks) {
+    var checkJson = checkHtmlContent(content, checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var cheerioHtmlContent = function(htmlcontent) {
+    return cheerio.load(htmlcontent);
 };
 
 var loadChecks = function(checksfile) {
@@ -45,7 +79,11 @@ var loadChecks = function(checksfile) {
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+    return checkHtmlContent(cheerioHtmlFile(htmlfile));
+};
+
+var checkHtmlContent = function(content, checksfile) {
+    $ = content;
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +103,17 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to remote index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    /*var checkJson = (program.url.length > 0)?checkHtmlFile(program.url, program.checks):checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    console.log(outJson);*/
+    if (program.url.length > 0) {
+      assertUrlExists(program.url, program.checks);
+    } 
+    else {
+      outputResult(cheerioHtmlFile(program.file), program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
